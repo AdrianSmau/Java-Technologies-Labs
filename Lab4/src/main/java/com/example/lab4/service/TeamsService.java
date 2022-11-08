75958a1ecfdf3541efec692c1721154781d5959f
AdrianSmau <smauadrian@gmail.com>
2022-11-08 17:27:41 +0200

    added homework component for lab4

--- Lab4/src/main/java/com/example/lab4/service/TeamsService.java
+++ Lab4/src/main/java/com/example/lab4/service/TeamsService.java
@@ -4,8 +4,10 @@ import com.example.lab4.entities.Team;
 
 import javax.annotation.ManagedBean;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import java.io.IOException;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -18,11 +20,29 @@ import java.util.Objects;
 @ManagedBean
 @RequestScoped
 public class TeamsService implements Serializable {
-    private static final String url = "jdbc:postgresql://localhost:5433/Lab3-Java";
-
     private String chosenCity;
     private String chosenFoundingDate;
     private String chosenName;
+
+    private static Connection getConnection() {
+        InitialContext ctx;
+        try {
+            ctx = new javax.naming.InitialContext();
+            javax.sql.DataSource ds = (javax.sql.DataSource) ctx.lookup("jdbc/demo");
+            return ds.getConnection();
+        } catch (NamingException | SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void goToManage() throws IOException {
+        FacesContext.getCurrentInstance().getExternalContext().redirect("manage.xhtml");
+    }
+
+    public void goToUpdate() throws IOException {
+        FacesContext.getCurrentInstance().getExternalContext().redirect("update.xhtml");
+    }
 
     public List<Team> getTeams() throws SQLException {
         return fetchTeamsFromDB(Objects.requireNonNull(getConnection()));
@@ -32,6 +52,14 @@ public class TeamsService implements Serializable {
         Connection conn = getConnection();
         assert conn != null;
         insertIntoDB(chosenName, chosenFoundingDate, chosenCity, conn);
+        conn.close();
+        return "/manage.xhtml?faces-redirect=true";
+    }
+
+    public String updateTeamAndCity() throws SQLException {
+        Connection conn = getConnection();
+        assert conn != null;
+        updateFromDB(chosenName, chosenFoundingDate, chosenCity, conn);
         conn.close();
         return "/manage.xhtml?faces-redirect=true";
     }
@@ -48,6 +76,34 @@ public class TeamsService implements Serializable {
         preparedStatement.setString(1, teamName);
         preparedStatement.setString(2, teamFoundationDate);
         preparedStatement.setString(3, city);
+
+        preparedStatement.execute();
+    }
+
+    private void updateFromDB(String teamName, String teamFoundationDate, String city, Connection conn) throws SQLException {
+        PreparedStatement preparedStatement = conn.prepareStatement("select * from cities where name=(?)");
+
+        preparedStatement.setString(1, city);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if (!resultSet.next()) {
+            preparedStatement = conn.prepareStatement("insert into cities values (?)");
+            preparedStatement.setString(1, city);
+            preparedStatement.execute();
+        }
+
+        preparedStatement = conn.prepareStatement("update teams set city=(?) where name=(?)");
+
+        preparedStatement.setString(1, city);
+        preparedStatement.setString(2, teamName);
+
+        preparedStatement.execute();
+
+        preparedStatement = conn.prepareStatement("update teams set \"foundingDate\"=(?) where name=(?)");
+
+        preparedStatement.setString(1, teamFoundationDate);
+        preparedStatement.setString(2, teamName);
 
         preparedStatement.execute();
     }
@@ -83,18 +139,6 @@ public class TeamsService implements Serializable {
             result.add(team);
         }
         return result;
-    }
-
-    private static Connection getConnection() {
-        InitialContext ctx;
-        try {
-            ctx = new javax.naming.InitialContext();
-            javax.sql.DataSource ds = (javax.sql.DataSource) ctx.lookup("jdbc/demo");
-            return ds.getConnection();
-        } catch (NamingException | SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
     }
 
     public String getChosenCity() {

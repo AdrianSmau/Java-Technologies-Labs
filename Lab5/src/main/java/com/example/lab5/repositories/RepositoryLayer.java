bf29547ba42f38a0da6af8f5442a64acc1e33666
AdrianSmau <smauadrian@gmail.com>
2022-12-04 20:42:16 +0200

    added compulsory and homework for Lab5

---
+++ Lab5/src/main/java/com/example/lab5/repositories/RepositoryLayer.java
@@ -0,0 +1,65 @@
+package com.example.lab5.repositories;
+
+import com.example.lab5.entities.City;
+import com.example.lab5.entities.Team;
+
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.UUID;
+
+@ManagedBean
+@RequestScoped
+@Transactional
+public class RepositoryLayer {
+    @PersistenceContext(unitName = "lab-5")
+    EntityManager em;
+
+    public void insertIntoDB(String teamName, String teamFoundationDate, String city) {
+        City cityToBeInserted = new City(UUID.randomUUID().toString(), city);
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        em.persist(cityToBeInserted);
+
+        Team teamToBeInserted = new Team(UUID.randomUUID().toString(), city, teamFoundationDate, teamName);
+        em.persist(teamToBeInserted);
+
+        et.commit();
+    }
+
+    public void updateFromDB(String teamName, String teamFoundationDate, String city) {
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        List<City> existentCity = em.createNativeQuery("SELECT * from cities where name=(?)", City.class).setParameter(1, city).getResultList();
+        if (existentCity.size() == 0) {
+            City cityToBeInserted = new City(UUID.randomUUID().toString(), city);
+            em.persist(cityToBeInserted);
+        }
+
+        em.createNamedQuery("updateTeamCity").setParameter(1, city).setParameter(2, teamName).executeUpdate();
+
+        em.createNamedQuery("updateTeamFounding").setParameter(1, teamFoundationDate).setParameter(2, teamName).executeUpdate();
+        et.commit();
+    }
+
+    public void deleteFromDB(String cityName) {
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        em.createNamedQuery("deleteTeam").setParameter(1, cityName).executeUpdate();
+
+        em.createNamedQuery("deleteCity").setParameter(1, cityName).executeUpdate();
+        et.commit();
+    }
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+    public List<Team> fetchTeamsFromDB() {
+        return em.createNativeQuery("SELECT * FROM teams", Team.class).getResultList();
+    }
+}
